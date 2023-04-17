621844c33b35aae2d1a506362523efe86bf370e4
shivacheripally <125255635+shivacheripally@users.noreply.github.com>
2023-04-17 21:39:21 +0530

    passing props to TaskView

--- src/components/SevenDays.js
+++ src/components/SevenDays.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './SevenDays.css';
-function SevenDays() {
+function SevenDays(props) {
   const [days, setDays] = useState([]);
 
   useEffect(() => {
@@ -13,16 +13,16 @@ function SevenDays() {
     }
     setDays(daysArr);
   }, []);
-  const color ='red';
+
   return (
-    <div>
+    <div className='container'>
       {days.map((day, index) => (
         <div className='date-box'>
           <div key={index}>{day}</div>
           <div className="icons">
-            <span><i class="fa-solid fa-check"></i></span>
-            <span><i class="fa-solid fa-xmark"></i></span>
-            <span><i class="fa-regular fa-circle-dot"></i></span>
+            <span onClick={(value)=>props.isClicked('done')}><i class="fa-solid fa-check"></i></span>
+            <span onClick={(value)=>props.isClicked('notdone')}><i class="fa-solid fa-xmark"></i></span>
+            <span onClick={(value)=>props.isClicked('notseen')}><i class="fa-regular fa-circle-dot"></i></span>
           </div>
           <div className='status'><span>Staus: Not Seen</span></div>
         </div>
--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import {data} from './index.js';
+import React,{useState} from 'react';
+import {data, SevenDays} from './index.js';
 import "./TaskView.css";
 
 function TaskView(props){
   var isVisible = "hide";
   var index = props.index;
-  
+  const [clicked,setClicked] = useState(false);
+
+  const isClicked=(value)=>{
+    setClicked(!clicked);
+    console.log(value);
+  }
+
   if(props.menuVisibility){
     isVisible = "show";
   }
 
   return(
-     <div onMouseDown={props.clickEvent} id="menu" className={isVisible}>
+     <div id="menu" className={isVisible}>
+       <button onMouseDown={props.clickEvent}>Close</button>
        <h1>{data[index]}</h1>
+       <SevenDays isClicked={isClicked}/>
      </div>
   );
 }
