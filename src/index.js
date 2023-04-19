51fa82a108eab14ff4c4c6c153aee868d3613847
shivacheripally <125255635+shivacheripally@users.noreply.github.com>
2023-04-19 07:59:40 +0530

    created redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React, { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import {createStore} from 'redux';
+import Status from './reducers';
 import App from './components/App';
+
+const store = createStore(Status);
+console.log('store',store);
+console.log('initial State: ',store.getState());
 
 
 const rootElement = document.getElementById('root');
